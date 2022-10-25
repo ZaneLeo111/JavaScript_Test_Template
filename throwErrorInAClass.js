356982dffdf31f5b0bb7f41674393bf714cb9213
ZeLiu1 <zliu17@mun.ca>
2022-10-25 12:36:45 -0230

    The throw error passes through the timeout

--- throwErrorInAClass.js
+++ throwErrorInAClass.js
@@ -1,10 +1,17 @@
 class test_class {
-  constructor() {}
+  constructor() {
+    this.searchStartTime = 0;
+
+    this.timeLimit = 0.0000001;
+  }
 
   tryAndCatch() {
+    this.searchStartTime = performance.now();
+
+    console.log("this.searchStartTime", this.searchStartTime);
     try {
-      this.tryFunc("asd");
-      this.tryFunc(1);
+      console.log("It tried");
+      this.testIfTimeout();
     } catch (error) {
       if (error.name == "TimeOutError") {
         console.error(error);
@@ -12,15 +19,34 @@ class test_class {
     }
   }
 
-  tryFunc(a) {
-    if (a != "asd") {
+  // tryFunc(a) {
+  //   if (a != "asd") {
+  //     throw { name: "TimeOutError", message: "Time out!" };
+  //   }
+
+  //   console.log("It works!");
+  // }
+
+  testIfTimeout() {
+    console.log('asdads');
+    let timeElapsed = performance.now() - this.searchStartTime;
+
+    console.log(" ------------------------------------------------");
+    console.log("test_ ~ testIfTimeout ~ timeElapsed", timeElapsed);
+    console.log(" ------------------------------------------------");
+
+
+    
+
+    if (timeElapsed > this.timeLimit) {
       throw { name: "TimeOutError", message: "Time out!" };
     }
-
-    console.log("It works!");
   }
 }
 
+ 
 // test run
 test1 = new test_class();
 test1.tryAndCatch();
+
+
